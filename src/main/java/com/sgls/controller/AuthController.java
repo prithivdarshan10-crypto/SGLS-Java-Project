538a3790f30157f0e1cfcf0741f90f0267b336f2
PRIYADARSHAN S V <prithivdarshan10@gmail.com>
2026-06-11 01:38:56 +0530

    Update AuthController.java

--- src/main/java/com/sgls/controller/AuthController.java
+++ src/main/java/com/sgls/controller/AuthController.java
@@ -36,7 +36,6 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ApiResponse<String>> register(
             @Valid @RequestBody RegisterRequest registerRequest) {
 
